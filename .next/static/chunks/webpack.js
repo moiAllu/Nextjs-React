27422a68084dbc99f01af91eda72b9028252399e
MoiAllu <al5690410@gmail.com>
2022-03-29 16:34:14 +0500

    v2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fca6cb03baf3417f0c0b";
+/******/ 	var hotCurrentHash = "92f67b96aa6725ada17b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout';
-import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   return <Layout> <Component {...pageProps} /></Layout>
 }
