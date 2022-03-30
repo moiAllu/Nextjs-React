18109ddfaf1228732ef55978635aba2da71676d9
MoiAllu <al5690410@gmail.com>
2022-03-30 14:46:50 +0500

    V3

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -5,14 +5,14 @@ function MainNavigation() {
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>CarX</div>
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link>
+            <Link href='/'>Home</Link>
           </li>
           <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
+            <Link href='/new-meetup'>Add new Car</Link>
           </li>
         </ul>
       </nav>
