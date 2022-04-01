3eeb5b7f00e4d6ba85fe9601380fe676bccb1363
MoiAllu <al5690410@gmail.com>
2022-04-01 16:19:57 +0500

    v5

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -5,7 +5,7 @@ const newDescription = () => {
     <NewDetailPage
       image="https://c4.wallpaperflare.com/wallpaper/451/732/57/nissan-gtr-r35-red-car-wallpaper-preview.jpg"
       title="Nissan GTR R34"
-      description="A rocket bunny kit attached to 2019 Gtr, where Hks mid pige,Hks turbo,Hks ECU, has been upgraded"
+      description="A rocket bunny kit attached to 2019 Gtr, where Hks mid pige,Hks turbo,Hks ECU, has been upgraded."
       address="Malborne, Australia"
     />
   );
